350a68e683c43342a6f59d3267001756e11dd58e
Joseph Schito <joseph.schito@gmail.com>
2025-07-24 15:41:48 +0200

    Import project

---
+++ lib/opal_proxy.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "opal_proxy/version"
+
+module OpalProxy
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/opal_proxy/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module OpalProxy
+  VERSION = "0.1.0"
+end
